17559b6f992a4643ffa3f4dcd84a99ebe1d809cd
omerozgur23 <omerozgur23@gmail.com>
2024-02-05 23:24:55 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Business.Concrete;
+using Entities.Concrete;
+
+Person person1 = new Person();
+person1.Firstname = "Ömer";
+person1.Lastname = "Özgür";
+person1.DateOfBirthYear = 1998;
+person1.NationalIdentity = 123;
+
+PttManager pttManager = new PttManager(new PersonManager);
+pttManager.GiveMask(person1);
+
+Console.ReadLine();
